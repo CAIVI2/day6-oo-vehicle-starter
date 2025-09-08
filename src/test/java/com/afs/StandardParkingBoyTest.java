18473fd1e7c91d4dcde649404237cc1bd1532ddd
VICTOR <victor.cai@oocl.com>
2025-09-08 18:41:39 +0800

    feat: return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_parking_lot_with_two_parked_cars_and_a_standard_parking_boy_and_two_parking_tickets

--- src/test/java/com/afs/StandardParkingBoyTest.java
+++ src/test/java/com/afs/StandardParkingBoyTest.java
@@ -28,4 +28,20 @@ public class StandardParkingBoyTest {
         assertEquals(car, fetchedCar);
     }
 
+    @Test
+    void should_return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_parking_lot_with_two_parked_cars_and_a_standard_parking_boy_and_two_parking_tickets() {
+        Car car1 = new Car("C1");
+        Car car2 = new Car("C2");
+        ParkingLot parkingLot = new ParkingLot(10);
+        StandardParkingBoy boy = new StandardParkingBoy(parkingLot);
+
+        Ticket ticket1 = boy.park(car1);
+        Ticket ticket2 = boy.park(car2);
+        Car fetchedCar1 = boy.fetch(ticket1);
+        Car fetchedCar2 = boy.fetch(ticket2);
+
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+    }
+
 }
