cc81b508bb298dc8303a03159af15da521130993
VICTOR <victor.cai@oocl.com>
2025-09-08 16:45:58 +0800

    feat: return_the_packed_car_when_fetch_the_car_given_a_parking_lot_with_a_parked_car

--- src/main/java/com/afs/ParkingLot.java
+++ src/main/java/com/afs/ParkingLot.java
@@ -1,13 +1,23 @@
 package com.afs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private final Map<Ticket, Car> ticketCars = new HashMap<>();
 
     public ParkingLot(int i) {
 
     }
 
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        ticketCars.put(ticket, car);
+        return ticket;
+    }
+
+    public Car fetch(Ticket ticket) {
+        return ticketCars.remove(ticket);
     }
 }
 
--- src/test/java/com/afs/ParkingLotTest.java
+++ src/test/java/com/afs/ParkingLotTest.java
@@ -1,6 +1,7 @@
 package com.afs;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -12,6 +13,17 @@ public class ParkingLotTest {
         Ticket ticket = parkingLot.park(car);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_the_packed_car_when_fetch_the_car_given_a_parking_lot_with_a_parked_car() {
+        Car car = new Car("C0");
+        ParkingLot parkingLot = new ParkingLot(10);
+        Ticket ticket = parkingLot.park(car);
+
+        Car fetchedCar = parkingLot.fetch(ticket);
+
+        assertEquals(car, fetchedCar);
     }
 }
 
